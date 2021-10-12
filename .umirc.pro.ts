74c2dac291a7c6b2a75bea0355769c94197ca4fe
jvbf2e <vvimoink@gmail.com>
2021-10-12 14:57:04 +0800

    init(初始化): demo
    demo

---
+++ .umirc.pro.ts
@@ -0,0 +1,16 @@
+/*
+ * @Author: jvb
+ * @LastEditors: jvb
+ * @email: tusktalk@163.com
+ * @github: https://github.com/jvbf2e
+ * @Date: 2021-10-11 10:58:07
+ * @LastEditTime: 2021-10-12 14:17:00
+ * @FilePath: \Developmente:\Joints\Project\blog\.umirc.pro.ts
+ */
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  define: {
+    API_PREFIX: '',
+  },
+});
